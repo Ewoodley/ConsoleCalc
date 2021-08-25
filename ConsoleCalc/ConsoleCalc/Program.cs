08d28458b7f841f07e93e2408fda3d0df01ba06c
Eric Woodley <noctisumbra@gmail.com>
2021-08-25 01:08:14 -0500

    Work done on input checking

--- ConsoleCalc/ConsoleCalc/Program.cs
+++ ConsoleCalc/ConsoleCalc/Program.cs
@@ -8,30 +8,39 @@ namespace ConsoleCalc
 
         static void Main(string[] args)
         {
-            //Blocking code to establish the method for value input for the calculator
+
             int num1 = 0;
             int num2 = 0;
-            //Int32 result;
+            Int32 result;
 
-           
-                Console.WriteLine("Welcome to the Console Calculator\n Please enter your first number:");
-                num1 = Convert.ToInt32(Console.ReadLine());
-            
-                                    Console.WriteLine("Please enter your second number");
-                    num2 = Convert.ToInt32(Console.ReadLine());
-               
-           /*         Console.WriteLine("You did not enter a number, please enter a number");
-                    Console.ReadLine();
             if (Int32.TryParse(Console.ReadLine(), out result))
             {
+                
+
+
+                Console.WriteLine("Welcome to the Console Calculator\n Please enter your first number:");
+                num1 = Convert.ToInt32(Console.ReadLine(), result);
+
+
+
+                Console.WriteLine($"{result} is not a number, please enter a number");
+
+
+
+                Console.WriteLine("Please enter your second number");
+                num2 = Convert.ToInt32(Console.ReadLine(), result);
+
+
+
+                Int32.TryParse(Console.ReadLine(), out result);
                 Console.WriteLine($"{result} is not a number, please enter a number");
             }
 
-            
 
-                        else
-            {
-             */   //Blocking code to run operand check to perform calculation and output result
+
+
+
+
                 Console.WriteLine("Please enter your operand");
                 Console.WriteLine("Add for Addition");
                 Console.WriteLine("Sub for Subtraction");
@@ -40,35 +49,35 @@ namespace ConsoleCalc
 
                 switch (Console.ReadLine())
                 {
-                case "Add":
-                    Console.WriteLine($"Your result is {num1 + num2}");
-                    break;
-                case "add":
-                    Console.WriteLine($"Your result is {num1 + num2}");
-                    break;
-                case "Sub":
-                    Console.WriteLine($"Your result is {num1 - num2}");
-                    break;
-                case "sub":
-                    Console.WriteLine($"Your result is {num1 - num2}");
-                    break;
-                case "Mul":
-                    Console.WriteLine($"Your result is {num1 * num2}");
-                    break;
-                case "mul":
-                    Console.WriteLine($"Your result is {num1 * num2}");
-                    break;
-                case "Div":
-                    Console.WriteLine($"Your result is {num1 / num2}");
-                    break;
-                case "div":
-                    Console.WriteLine($"Your result is {num1 / num2}");
-                    break;
+                    case "Add":
+                        Console.WriteLine($"Your result is {num1 + num2}");
+                        break;
+                    case "add":
+                        Console.WriteLine($"Your result is {num1 + num2}");
+                        break;
+                    case "Sub":
+                        Console.WriteLine($"Your result is {num1 - num2}");
+                        break;
+                    case "sub":
+                        Console.WriteLine($"Your result is {num1 - num2}");
+                        break;
+                    case "Mul":
+                        Console.WriteLine($"Your result is {num1 * num2}");
+                        break;
+                    case "mul":
+                        Console.WriteLine($"Your result is {num1 * num2}");
+                        break;
+                    case "Div":
+                        Console.WriteLine($"Your result is {num1 / num2}");
+                        break;
+                    case "div":
+                        Console.WriteLine($"Your result is {num1 / num2}");
+                        break;
                 }
-                 
+
+
+
             
-           
-        }
-           
+        }  
     }
 }
