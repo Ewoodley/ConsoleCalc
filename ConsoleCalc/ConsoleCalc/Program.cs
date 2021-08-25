ef254ed92c5c539fdd0c722ce92ee6a4fcaa3dd3
Eric Woodley <noctisumbra@gmail.com>
2021-08-24 23:11:04 -0500

    Add container for number inputs, and calculator functions

--- ConsoleCalc/ConsoleCalc/Program.cs
+++ ConsoleCalc/ConsoleCalc/Program.cs
@@ -5,42 +5,53 @@ namespace ConsoleCalc
 {
     class Program
     {
-        public static int Int { get; private set; }
 
         static void Main(string[] args)
         {
+            //Blocking code to establish the method for value input for the calculator
+            int num1 = 0;
+            int num2 = 0;
+            string[] operand = {"A", "S", "M", "D"};
 
-            int[] input = new int[4]; //Number storage for calculator function
 
-            Console.WriteLine("Welcome to the Console Calculator\n Please enter your first number:");
-            Console.ReadLine();
-            if (input[0] == Int)
-            {
-                Console.WriteLine("Please enter your second number");
-                Console.ReadLine();
-            }
-                         
-            else
-            {
-                Console.WriteLine("You did not enter a number, please enter a number");
-                Console.ReadLine();
-            }
-
-            Console.Writeline("Please enter your operand(+, -, * or /)");
-            Console.ReadLine();
-            if (input[2]= '+')
+                Console.WriteLine("Welcome to the Console Calculator\n Please enter your first number:");
+                num1 = Convert.ToInt32(Console.ReadLine());
+            if (num1 = Int)
                 {
-                input[0] += input[1]
-                    }
-                    if (input[2] = '-')
-                    input[0] -= input[1];
-            if (input[2] = '*')
-                input[0] *= input[1];
-            if (input[2] = '/')
-                input[0] /= input[1];
-            Console.WriteLine($"Your result is {input[3]}");
+                    Console.WriteLine("Please enter your second number");
+                    num2 = Convert.ToInt32(Console.ReadLine());
+                }
 
+                else
+                {
+                    Console.WriteLine("You did not enter a number, please enter a number");
+                    Console.ReadLine();
+                }
+                //Blocking code to run operand check to perform calculation and output result
+                Console.WriteLine("Please enter your operand");
+                Console.WriteLine("Add for Addition");
+                Console.WriteLine("Sub for Subtraction");
+                Console.WriteLine("Mul for Multiply");
+                Console.WriteLine("Div for Divide");
+                Console.ReadLine();
+                switch(operand)
+                {
+                case A:
+                    num1 += num2;
+                case S:
+                    num1 -= num2;
+                case M:
+                    num1 *= num2;
+                case D:
+                    num1 /= num2;
+                    
 
+            
+            
+                }
+                Console.WriteLine($"Your result is {input[2]}");
+
+           
         }
            
     }
