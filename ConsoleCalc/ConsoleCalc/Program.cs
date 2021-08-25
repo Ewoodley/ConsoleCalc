94eaf961384cd2ff991eda7b59fc6290767dbb1a
Eric Woodley <noctisumbra@gmail.com>
2021-08-25 00:25:43 -0500

    Number inputs working correct, math functions now working

--- ConsoleCalc/ConsoleCalc/Program.cs
+++ ConsoleCalc/ConsoleCalc/Program.cs
@@ -11,46 +11,62 @@ namespace ConsoleCalc
             //Blocking code to establish the method for value input for the calculator
             int num1 = 0;
             int num2 = 0;
-            string[] operand = {"A", "S", "M", "D"};
+            //Int32 result;
 
-
+           
                 Console.WriteLine("Welcome to the Console Calculator\n Please enter your first number:");
                 num1 = Convert.ToInt32(Console.ReadLine());
-            if (num1 = Int)
-                {
-                    Console.WriteLine("Please enter your second number");
+            
+                                    Console.WriteLine("Please enter your second number");
                     num2 = Convert.ToInt32(Console.ReadLine());
-                }
-
-                else
-                {
-                    Console.WriteLine("You did not enter a number, please enter a number");
+               
+           /*         Console.WriteLine("You did not enter a number, please enter a number");
                     Console.ReadLine();
-                }
-                //Blocking code to run operand check to perform calculation and output result
+            if (Int32.TryParse(Console.ReadLine(), out result))
+            {
+                Console.WriteLine($"{result} is not a number, please enter a number");
+            }
+
+            
+
+                        else
+            {
+             */   //Blocking code to run operand check to perform calculation and output result
                 Console.WriteLine("Please enter your operand");
                 Console.WriteLine("Add for Addition");
                 Console.WriteLine("Sub for Subtraction");
                 Console.WriteLine("Mul for Multiply");
                 Console.WriteLine("Div for Divide");
-                Console.ReadLine();
-                switch(operand)
+
+                switch (Console.ReadLine())
                 {
-                case A:
-                    num1 += num2;
-                case S:
-                    num1 -= num2;
-                case M:
-                    num1 *= num2;
-                case D:
-                    num1 /= num2;
-                    
-
-            
-            
+                case "Add":
+                    Console.WriteLine($"Your result is {num1 + num2}");
+                    break;
+                case "add":
+                    Console.WriteLine($"Your result is {num1 + num2}");
+                    break;
+                case "Sub":
+                    Console.WriteLine($"Your result is {num1 - num2}");
+                    break;
+                case "sub":
+                    Console.WriteLine($"Your result is {num1 - num2}");
+                    break;
+                case "Mul":
+                    Console.WriteLine($"Your result is {num1 * num2}");
+                    break;
+                case "mul":
+                    Console.WriteLine($"Your result is {num1 * num2}");
+                    break;
+                case "Div":
+                    Console.WriteLine($"Your result is {num1 / num2}");
+                    break;
+                case "div":
+                    Console.WriteLine($"Your result is {num1 / num2}");
+                    break;
                 }
-                Console.WriteLine($"Your result is {input[2]}");
-
+                 
+            
            
         }
            
